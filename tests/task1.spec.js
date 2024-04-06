f7136648b6cc960942749fb3c9d235034256c250
ahumaniuk <a.humaniuk@gmail.com>
2024-04-06 21:57:17 +0200

    test in progress

--- tests/task1.spec.js
+++ tests/task1.spec.js
@@ -1,18 +1,31 @@
-const {test, expect } = require('playwright/test')
+const { expect } = require('playwright/test')
+
+
+
+
+
+
+const { test } = require('...src\fixture.js');
+
 
 //import { test, expect } from '@playwright/test';
 
 test.describe('Test Suite1', () => {
-  let page;
 
-test.beforeEach(async ({ page }) => {
+test.beforeEach(async ({ loginPage }) => {
   // Go to the starting url before each test and perform login
+  /*
   await page.goto('');
   await page.locator("#user-name").click();
   await page.locator("#user-name").fill('standard_user');
   await page.locator("#password").click();
   await page.locator("#password").fill('secret_sauce');
   await page.locator("#login-button").click();
+  */
+  await loginPage.navigate();
+  await loginPage.performLogin('standard_user', 'secret_sauce');
+
+
 });
 
 test.afterEach(async ({page}) =>{
@@ -20,22 +33,21 @@ test.afterEach(async ({page}) =>{
 });
 
 test('ID=1; Verify sorting ASC ', async ({ page }) => {
-  //await page.locator(".product_sort_container").click();
-  await page.locator("select.product_sort_container").click();
-  await page.locator('.select_container option[value="za"]').click();
+  await page.locator('select.product_sort_container').selectOption('Name (Z to A)');
 
-  
-  
-  //await page.locator(".inventory_item_name").textContent();
-  const inventoryItemsNames = await page.$$('.inventory_item_name');
-  const arrayOfNames = await Promise.all(inventoryItemsNames.map(async (element) => {
-    return await element.textContent();
-  }));
-  //performSorting('az')
-  arrayOfNames.then(data => {
-    data.sort((a, b) => a.name.localeCompare(b.name));
-    console.log(data);
+const inventoryItemNames = await page.$$eval('.inventory_item_name', elements =>
+    elements.map(element => element.textContent.trim())
+  );
 
-})
+const sortedInventoryItemNames = inventoryItemNames.slice().sort((a, b) => {
+    if (a < b) return 1;
+    if (a > b) return -1;
+    return 0;
+});
+  
+const isSortedDesc = JSON.stringify(sortedInventoryItemNames) === JSON.stringify(inventoryItemNames.slice().sort().reverse());
+expect(isSortedDesc).toEqual(true)
+expect(isSortedDesc).toBeTruthy()
+  
 });
 })
